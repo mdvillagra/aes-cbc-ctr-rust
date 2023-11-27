9f7bfad795d50047929b59dfc0db4e1bdf58d2b4
mdvillagra <mdvillagra@gmail.com>
2023-11-27 10:44:47 -0300

    working cbc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use aes::cipher::{generic_array::GenericArray, BlockCipher, BlockDecrypt, BlockEncrypt, KeyInit};
+use aes::cipher::{generic_array::GenericArray, BlockEncrypt, KeyInit};
 use aes::Aes128;
-use hex::{FromHex, ToHex};
+use hex::FromHex;
 use rand::Rng;
 
 /// PKCS5 padding of message m
@@ -29,11 +29,18 @@ fn xor_16bytes_hex(a: String, b: String) -> String {
     assert_eq!(a.len(), 32, "a must be 16 bytes long");
     assert_eq!(b.len(), 32, "b must be 16 bytes long");
 
-    a.chars()
-        .zip(b.chars())
-        .map(|(x, y)| x as u8 ^ y as u8)
-        .map(|u| hex::encode(u.to_string()))
-        .fold("".to_string(), |acc, n| format!("{acc}{n}"))
+    let a_bytes = hex::decode(a).unwrap();
+    let b_bytes = hex::decode(b).unwrap();
+
+    let xor = a_bytes
+        .iter()
+        .zip(b_bytes.iter())
+        .map(|(x, y)| x ^ y)
+        .collect::<Vec<u8>>();
+
+    xor.iter().fold("".to_string(), |acc, n| {
+        format!("{acc}{}", hex::encode(vec![*n]))
+    })
 }
 /// CBC mode with AES128
 fn cbc_aes128(m: String, k: String) -> String {
@@ -49,12 +56,11 @@ fn cbc_aes128(m: String, k: String) -> String {
 
     // generate random IV
     let mut rng = rand::thread_rng();
-    let random_iv = hex::encode([
-        rng.gen::<u8>(),
-        rng.gen::<u8>(),
-        rng.gen::<u8>(),
-        rng.gen::<u8>(),
-    ]);
+    let random_bytes = vec![0; 16]
+        .iter()
+        .map(|_| rng.gen::<u8>())
+        .collect::<Vec<u8>>();
+    let random_iv = hex::encode(random_bytes);
 
     // string for the ciphertext
     let mut c = Vec::<String>::new();
@@ -62,6 +68,7 @@ fn cbc_aes128(m: String, k: String) -> String {
     // push the IV as the first element in the ciphertext
     c.push(random_iv.clone());
 
+    // encrypt each block
     for i in 0..padded_message.len() / 32 {
         let block_xor = xor_16bytes_hex(
             c[c.len() - 1].clone(),
@@ -77,6 +84,7 @@ fn cbc_aes128(m: String, k: String) -> String {
 
 fn main() {
     let c1 = String::from("4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81");
+    let key1 = String::from("140b41b22a29beb4061bda66b6747e14");
 
-    println!("{}", c1.len());
+    println!("{}", cbc_aes128(c1, key1));
 }
